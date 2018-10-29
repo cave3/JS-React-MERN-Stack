3c6945f672030548be20ce9d65176a24cf837278
wrk <wrks@mac.com>
2018-10-29 10:22:50 +0100

    added API server, model and routes

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    "mongodb://cave3:cave3dev@ds145093.mlab.com:45093/cave3-mean-shopping"
+};
+
+// mongo ds145093.mlab.com:45093/cave3-mean-shopping -u cave3 -p cave3dev
